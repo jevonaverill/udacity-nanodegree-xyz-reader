b6146658c4f40dbf00dc1222a674eaf5b1230afc
Jevon Averill <jevonaverill@Jevons-MacBook-Pro.local>
2018-06-25 21:56:26 +0700

    code fixing based on existing reviews and code reformatting

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -1,25 +1,29 @@
 package com.example.xyzreader.remote;
 
-import android.util.Log;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.example.xyzreader.BuildConfig;
+
+import android.util.Log;
+
 public class Config {
-    public static final URL BASE_URL;
-    private static String TAG = Config.class.toString();
+  public static final URL BASE_URL;
+  private static String TAG = Config.class.toString();
 
     static {
-        URL url = null;
-        try {
-            // TODO: change with new one
-            url = new URL("https://raw.githubusercontent" +
-                    ".com/jevonaverill/udacity-nanodegree-xyz-reader/master/data.json");
-        } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
-            Log.e(TAG, "Please check your internet connection.");
-        }
-
-        BASE_URL = url;
+    URL url = null;
+    try {
+      // TODO: change with new one
+      url = new URL("https://raw.githubusercontent"
+          + ".com/jevonaverill/udacity-nanodegree-xyz-reader/master/data.json");
+    } catch (MalformedURLException ignored) {
+      // TODO: throw a real error
+      if (BuildConfig.DEBUG) {
+        Log.e(TAG, "Please check your internet connection.");
+      }
     }
+
+    BASE_URL = url;
+  }
 }
